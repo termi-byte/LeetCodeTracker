29573d100d4c08fc0c4a3bf5c0822fbfbad6f4d8
termi-byte <terminatortixmul@gmail.com>
2026-07-11 00:16:43 +0530

    Time: 0 ms (100%), Space: 10.1 MB (93.79%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,12 +3,13 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int row=matrix.size();
         int col=matrix[0].size();
+        //transpose
         for(int i=0;i<row;i++){
             for(int j=i+1;j<col;j++){        
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
-
+        //reverse
         for(int i=0;i<row;i++){
             reverse(matrix[i].begin(),matrix[i].end());
         }
